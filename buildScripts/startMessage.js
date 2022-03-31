25c3938de9ffe627600e6eaa9b92aab16ae93595
Farhad Siraj <farhad.siraj@ibm.com>
2022-03-31 12:57:28 -0400

    Build: Set up Babel Transpiling

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,6 @@
-var chalk = require("chalk");
+// const chalk = require("chalk");
+
+// Babel allows you to use import instead of require in backend code
+import chalk from "chalk";
 
 console.log(chalk.green("Starting app in dev mode..."));
