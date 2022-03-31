238207593590b7a428dd583d129d5fa0e42f8d15
Farhad Siraj <farhad.siraj@ibm.com>
2022-03-31 16:36:06 -0400

    Build: Set up ESLint

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -3,4 +3,4 @@
 // Babel allows you to use import instead of require in backend code
 import chalk from "chalk";
 
-console.log(chalk.green("Starting app in dev mode..."));
+console.log(chalk.green("Starting app in dev mode...")); // eslint-disable-line no-console
